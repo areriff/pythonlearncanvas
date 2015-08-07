b8fc7ef7168628f09744b42990fb961df1d360cd
areriff <areriff@users.noreply.github.com>
2015-08-08 03:46:20 +0800

    nothing, just add few things

---
+++ firstMain.py
@@ -0,0 +1,7 @@
+__author__ = 'areriff'
+print 'Hello World'
+area = 0
+height = 10
+width = 20
+area = width * height / 2  # calculate the area of a triangle
+print 'the area of the triangle would be %3d kilometre square' % area
