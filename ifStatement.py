0012b2ec19de6515db722b90bb04bd14f853fe4b
areriff <arif_nfbmm@hotmail.com>
2015-08-08 17:23:04 +0800

    the if statement tutorial v1

--- ifStatement.py
+++ ifStatement.py
@@ -1,7 +1,17 @@
 __author__ = 'arif_'
-answer = input('would you like express shipping')
-if answer == 'yes':
-    print('that will be an extra $10')
+# answer = input('would you like express shipping')
+# if answer == 'yes':
+#     print('that will be an extra $10')
+# else:
+#     print('fine!')
+# print('have a nice day')
+favouriteTeam = input('what is your favourite hockey team?\n')
+if favouriteTeam == 'Senators':
+    print('Yeah Go Sens Go')
+    print('Hmm')
 else:
-    print('fine!')
-print('have a nice day')
+    print('bleh')
+if favouriteTeam == 'MU':
+    print('Yeah')
+else:
+    print('boo')
