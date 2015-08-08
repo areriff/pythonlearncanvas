a9b44637eb94944aab5141c1e33c61c1681fc6b8
areriff <arif_nfbmm@hotmail.com>
2015-08-08 17:17:00 +0800

    the if statement tutorial v1

--- ifStatement.py
+++ ifStatement.py
@@ -2,3 +2,6 @@ __author__ = 'arif_'
 answer = input('would you like express shipping')
 if answer == 'yes':
     print('that will be an extra $10')
+else:
+    print('fine!')
+print('have a nice day')
